e49b498dd3caeb6fc22831fe06f8d89dc24816d8
Czerwo <mateczerw@gmail.com>
2019-09-02 20:23:43 +0200

    First project CRUD

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemmo/ThymeleafdemmoApplication.java
@@ -0,0 +1,13 @@
+package com.luv2code.springboot.thymeleafdemmo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafdemmoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafdemmoApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/luv2code/springboot/thymeleafdemmo/service/EmployeeService.java
@@ -0,0 +1,15 @@
+package com.luv2code.springboot.thymeleafdemmo.service;
+
+import java.util.List;
+
+import com.luv2code.springboot.thymeleafdemmo.entity.Employee;
+
+public interface EmployeeService {
+	public List<Employee> findAll();
+	
+	public Employee findById(int theId);
+	
+	public void save(Employee theEmployee);
+	
+	public void deleteById(int theId);
+}
